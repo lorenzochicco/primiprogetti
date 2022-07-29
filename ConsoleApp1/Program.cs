fcc2f051cf6c102f5cf68d542e44d3e6cab4588b
lorenzochicco <110227657+lorenzochicco@users.noreply.github.com>
2022-07-29 11:26:55 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿int num1 = 0; int num2 = 0;
+Console.WriteLine("Questo è un calcolatore in c#\r");
+Console.WriteLine("------------------------\n");
+Console.WriteLine("Inserisci un numero e premi invio");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inserisci un altro numero e premi invio");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Scegli che operatore utilizzare tra i seguenti e premi invio");
+Console.WriteLine(" \ta = somma");
+Console.WriteLine(" \ts = sottrazione");
+Console.WriteLine(" \tm = moltiplicazione");
+Console.WriteLine(" \td = divisione");
+Console.Write("Cosa scegli? ");
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+Console.WriteLine("Press any key to close the Calculator console app...");
+Console.ReadKey();
